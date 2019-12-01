c472bb92440371aadf87e7560753d3318a1c9017
krausg <guenter.kraus@mailbox.org>
2019-12-01 23:55:45 +0100

    more aoc unit tests added

---
+++ src/test/java/aoc/puzzle01/FuelRequirementsLineProcessorTest.java
@@ -0,0 +1,32 @@
+package aoc.puzzle01;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import aoc.NumberLine;
+
+public class FuelRequirementsLineProcessorTest {
+
+	FuelRequirementsLineProcessor processor = new FuelRequirementsLineProcessor();
+
+	@Test
+	public void aocTest1() {
+		Assert.assertEquals(2, processor.process(new NumberLine(12)).getLineAsInt());
+	}
+
+	@Test
+	public void aocTest2() {
+		Assert.assertEquals(2, processor.process(new NumberLine(14)).getLineAsInt());
+	}
+
+	@Test
+	public void aocTest3() {
+		Assert.assertEquals(654, processor.process(new NumberLine(1969)).getLineAsInt());
+	}
+
+	@Test
+	public void aocTest4() {
+		Assert.assertEquals(33583, processor.process(new NumberLine(100756)).getLineAsInt());
+	}
+
+}

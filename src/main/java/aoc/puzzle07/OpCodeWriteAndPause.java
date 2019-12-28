57dd10b22b70388d646fb9431f817730a85437aa
krausg <guenter.kraus@mailbox.org>
2019-12-29 00:24:28 +0100

    made OpCodeWriteAndPause subclass of OpCodeWrite

--- src/main/java/aoc/puzzle07/OpCodeWriteAndPause.java
+++ src/main/java/aoc/puzzle07/OpCodeWriteAndPause.java
@@ -1,20 +1,14 @@
 package aoc.puzzle07;
 
-import aoc.puzzle05.IntCodeCmd;
 import aoc.puzzle05.IntCodeValue;
+import aoc.puzzle05.OpCodeWrite;
 
-public class OpCodeWriteAndPause implements IntCodeCmd {
+public class OpCodeWriteAndPause extends OpCodeWrite {
 
 	@Override
 	public void execute(aoc.puzzle05.IntCodeController pgm, IntCodeValue[] params) {
-		// pgm.resetOutputCache();
-		pgm.getOutput().append(params[0].value);
+		super.execute(pgm, params);
 		pgm.setPaused(true);
-	}
-
-	@Override
-	public int getOpCodeLength() {
-		return 2;
 	}
 
 }
